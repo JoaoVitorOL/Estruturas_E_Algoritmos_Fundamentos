900742ff9d77f8178b71f48d30fc7bfa440ad40e
João Vitor de Oliveira Lima <160860021+JoaoVitorOL@users.noreply.github.com>
2026-01-26 16:13:01 -0300

    Update BubbleSort.java

---
+++ algoritmos_ordenacao/BubbleSort.java
@@ -0,0 +1,55 @@
+package algoritmos_ordenacao;
+
+import java.util.Arrays;
+
+
+public class BubbleSort {
+
+    public static void bubbleSort(int[] array) {
+
+        int tamanho = array.length;
+
+        // Controla o número de voltas pelo array
+        for (int volta = 0; volta < tamanho - 1; volta++) {
+
+
+            // Percorre o vetor comparando elementos adjacentes
+            for (int indiceatual = 0; indiceatual < tamanho - 1 - volta; indiceatual++) {
+
+                // Se estiver fora de ordem, troca
+                if (array[indiceatual] > array[indiceatual + 1]) {
+
+                    int temp = array[indiceatual];
+                    array[indiceatual] = array[indiceatual + 1];
+                    array[indiceatual + 1] = temp;
+                }
+            }
+        }
+
+
+    }
+
+
+    /* =========================
+       MAIN PARA TESTE
+       ========================= */
+    public static void main(String[] args) {
+
+        int[] dados = LeitorArquivo.carregarArray("algoritmos_ordenacao/numeros_100k.txt");
+
+        System.out.println("Antes da ordenação:");
+
+
+        long inicio = System.nanoTime();
+        bubbleSort(dados);
+        long fim = System.nanoTime();
+        long duracao = fim - inicio;
+        double tempoMs = duracao / 1_000_000.0;
+        System.out.println("Tempo de execução (ms): " + tempoMs);
+
+
+        System.out.println("Depois da ordenação:");
+        System.out.println(Arrays.toString(dados));
+
+    }
+}
